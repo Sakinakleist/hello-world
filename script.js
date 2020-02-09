1437e066a58c1cc0fe133648cdab039dc7b5ca96
Sakina <sorenpetersson@Srens-MacBook-Air.local>
2020-02-09 15:46:02 +0100

    ændret side

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", start);
+
+function start() {
+
+    document.querySelector("body").classList.add("fade_in");
+    document.querySelector(".logo").classList.add("fade_in_slide");
+}
